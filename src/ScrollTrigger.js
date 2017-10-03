038b928d76ad8f78a00cf8bd9d50d2b3b5934bf3
Igormandello <igormandello@gmail.com>
2017-10-03 16:39:28 -0300

    Changes the ScrollManager class
    - Now the the section is added in a object/animation pair
    - Removes the parameters of the constructor, because of the addSection() function

--- src/ScrollTrigger.js
+++ src/ScrollTrigger.js
@@ -1,29 +1,29 @@
-function ScrollManager(a1, a2)
+function ScrollManager()
 {
-    for (let n = 0; n < a1.length; n++)
-    {
-        this.activated.push(false);
-        this.objects.push($(a1[n]));
-        this.animations.push(a2[n]);
-    }
-    
     this.scrollCheck = () =>
     {
         for (let n = 0; n < this.activated.length; n++)
             if (!this.activated[n])
-            {
-                if (this.objects[n].offset().top - $(window).scrollTop() <= $(window).height() * ScrollManager.offsetTop)
+                if (this.objects[n].offset().top - $(window).scrollTop() <= $(window).height() * (1 - ScrollManager.offsetTop))
                 {
+                    this.objects[n].removeClass('hidden');
+                    
                     this.animations[n]();
                     this.activated[n] = true;
                 }
-            }    
     }
     $(document).scroll(this.scrollCheck);
 }
 
-ScrollManager.prototype.activated = [];
+ScrollManager.prototype.activated  = [];
 ScrollManager.prototype.animations = [];
-ScrollManager.prototype.objects = [];
+ScrollManager.prototype.objects    = [];
 
-ScrollManager.offsetTop = 0.6;
+ScrollManager.prototype.addSection = function(strQuery, fn)
+{
+    this.objects.push($(strQuery));
+    this.activated.push(false);
+    this.animations.push(fn);
+}
+
+ScrollManager.offsetTop = 0.4;
