03b52a909b1f6a790f6925cd4479e9bd73894259
Igormandello <igormandello@gmail.com>
2017-10-03 23:42:56 -0300

    Add returnable section method
    - A returnable section calls the fn function everytime it exits the
    screen and everytime it appears on the screen
    - The fn function receives as a parameter, a boolean, indicating whether
    the section is active or not

--- src/ScrollTrigger.js
+++ src/ScrollTrigger.js
@@ -3,10 +3,27 @@ function ScrollManager()
     this.scrollCheck = () =>
     {
         for (let n = 0; n < this.activated.length; n++)
-            if (!this.activated[n])
-                if (this.objects[n].offset().top - $(window).scrollTop() <= $(window).height() * (1 - ScrollManager.offsetTop))
+            if (this.objects[n].returnable)
+              if (this.activated[n])
+              {
+                if (this.objects[n].object.offset().top + this.objects[n].object.height() - $(window).scrollTop() > 0)
                 {
-                    this.objects[n].removeClass('hidden');
+                  this.activated[n] = false;
+                  this.animations[n](this.activated[n]);
+                }    
+              }  
+              else
+              {
+                if (this.objects[n].object.offset().top + this.objects[n].object.height() - $(window).scrollTop() < 0)
+                {
+                    this.activated[n] = true;
+                    this.animations[n](this.activated[n]);
+                }
+              }
+            else if (!this.activated[n])
+                if (this.objects[n].object.offset().top - $(window).scrollTop() <= $(window).height() * (1 - ScrollManager.offsetTop))
+                {
+                    this.objects[n].object.removeClass('hidden');
                     
                     this.animations[n]();
                     this.activated[n] = true;
@@ -21,7 +38,14 @@ ScrollManager.prototype.objects    = [];
 
 ScrollManager.prototype.addSection = function(strQuery, fn)
 {
-    this.objects.push($(strQuery));
+    this.objects.push({object: $(strQuery), returnable: false});
+    this.activated.push(false);
+    this.animations.push(fn);
+}
+
+ScrollManager.prototype.addReturnableSection = function(strQuery, fn)
+{
+    this.objects.push({object: $(strQuery), returnable: true});
     this.activated.push(false);
     this.animations.push(fn);
 }
